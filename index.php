83a7ad8f19bad91a4d4a518f1cc5aa4be5111854
Kirthika Vijayakumar <kirthika@multicorewareinc.com>
2021-11-23 09:49:40 +0530

    add message

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php include_once("Inference.html"); ?>
+<?php include_once("Inference.html");
+SetEnvIf Origin ^(http?://m\.example\.com(?::\d{1,5})?)$   CORS_ALLOW_ORIGIN=$1
+Header append Access-Control-Allow-Origin  %{CORS_ALLOW_ORIGIN}e   env=CORS_ALLOW_ORIGIN
+Header set Access-Control-Allow-Methods "POST, GET, OPTIONS, DELETE, PUT"
+Header set Access-Control-Allow-Credentials true
+Header set Access-Control-Allow-Headers "x-requested-with, Content-Type, origin, authorization, accept, client-security-token"
+?>
